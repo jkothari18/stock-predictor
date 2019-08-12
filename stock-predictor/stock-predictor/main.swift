4a1c48553267daf234e572346bf9ba1c9d55de59
Ryan Elliott <ryelliot@calpoly.edu>
2019-08-12 07:38:51 -0700

    Inital commit

---
+++ stock-predictor/stock-predictor/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  stock-predictor
+//
+//  Created by Ryan Elliott on 8/12/19.
+//  Copyright © 2019 Ryan Elliott. All rights reserved.
+//
+
+import Foundation
+
+let url = URL(string: "https://sandbox.iexapis.com/stable/stock/aapl/quote?token=Tsk_c4ac493e5fce4aab9e71f9e911e5f482")!
+
+URLSession.shared.dataTask(with: url) {(data, response, error) in
+    guard let data = data else { return }
+    print(String(data: data, encoding: .utf8)!)
+}.resume()
+
+sleep(10)
