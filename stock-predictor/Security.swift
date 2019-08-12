f8d2dbeb5d8d9943532091e1a66a150e7d0abd1e
Ryan Elliott <relliot@skillz.com>
2019-08-12 15:31:33 -0700

    Added basic Security class

---
+++ stock-predictor/Security.swift
@@ -0,0 +1,14 @@
+//
+//  Security.swift
+//  stock-predictor
+//
+//  Created by Ryan Elliott on 8/12/19.
+//  Copyright © 2019 Ryan Elliott. All rights reserved.
+//
+
+import Foundation
+
+struct Security {
+    var ticker: String
+    var companyName: String
+}
